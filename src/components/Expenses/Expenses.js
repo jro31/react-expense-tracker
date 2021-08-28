8c716d4c7ef61336a875d505bf519f371d630024
Jethro Williams <jethro.williams@thirdspacelearning.com>
2021-08-28 21:07:36 +0100

    Create Card composition module, add eslintcache to gitignore, reorganise files and slightly update syntax

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,14 @@
 import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
-    <div className='expenses'>
+    <Card className='expenses'>
       {props.expenses.map(expense => (
         <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
       ))}
-    </div>
+    </Card>
   )
 }
 
