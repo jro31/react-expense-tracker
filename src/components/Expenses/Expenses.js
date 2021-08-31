0f33ba65a8f608f32c2a12f67213f99525ff72ad
Jethro Williams <jethro.williams@thirdspacelearning.com>
2021-08-31 21:00:54 +0100

    Assignment 2: Add an expense filter, and pass its state up to the Expenses component. Also includes update to NewExpense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2019');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  }
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       {props.expenses.map(expense => (
         <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
       ))}
